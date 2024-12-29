0f653e827e6770520e48591b6ce1a3be456ae8e6
Vinamra Garg <gargvinamra21@gmail.com>
2024-12-29 23:03:44 +0530

    Basic ui

---
+++ campuscloak/app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import LeftPanel from "./components/leftPanel";
+import RightPanel from "./components/rightPanel";
+export default function Home() {
+	const { setTheme } = useTheme();
+
+	return (
+		<main className='m-5'>
+      <div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
+        <div className='fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30' />
+        <LeftPanel />
+        <RightPanel />
+    </div>
+  </main>
+	);
+}
